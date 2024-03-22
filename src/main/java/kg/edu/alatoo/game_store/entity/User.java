7b162644de3534a12d79ce5579c224489abc0e15
Давид Остапченко <dsxvqq@gmail.com>
2024-03-22 23:42:07 +0600

    added lombok to user

--- src/main/java/kg/edu/alatoo/game_store/entity/User.java
+++ src/main/java/kg/edu/alatoo/game_store/entity/User.java
@@ -2,10 +2,18 @@ package kg.edu.alatoo.game_store.entity;
 
 import jakarta.persistence.Entity;
 import kg.edu.alatoo.game_store.enums.Role;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
 public class User extends BaseEntity {
 
     String nickname;
