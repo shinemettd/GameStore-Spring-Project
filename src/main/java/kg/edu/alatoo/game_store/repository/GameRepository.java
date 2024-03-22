f750464cf43776e8423b5b251a1349eb8e39e55c
Давид Остапченко <dsxvqq@gmail.com>
2024-03-22 20:27:44 +0600

    added repo annotation

--- src/main/java/kg/edu/alatoo/game_store/repository/GameRepository.java
+++ src/main/java/kg/edu/alatoo/game_store/repository/GameRepository.java
@@ -2,6 +2,8 @@ package kg.edu.alatoo.game_store.repository;
 
 import kg.edu.alatoo.game_store.entity.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface GameRepository extends JpaRepository<Long, Game> {
 }
