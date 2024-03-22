5830c4340f835c3071c5646977c958dc26294e07
Давид Остапченко <dsxvqq@gmail.com>
2024-03-22 20:18:41 +0600

    created base entities

---
+++ src/main/java/kg/edu/alatoo/game_store/entity/BaseEntity.java
@@ -0,0 +1,16 @@
+package kg.edu.alatoo.game_store.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+
+@MappedSuperclass
+@Data
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long Id;
+
+}
---
+++ src/main/java/kg/edu/alatoo/game_store/entity/Game.java
@@ -0,0 +1,22 @@
+package kg.edu.alatoo.game_store.entity;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class Game extends BaseEntity {
+
+    String title;
+
+    Double price;
+
+    Integer discount;
+
+}
