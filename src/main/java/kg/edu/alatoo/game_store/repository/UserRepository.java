170ec7323c2e4c63cc424b99fbb6dda320073171
Давид Остапченко <dsxvqq@gmail.com>
2024-03-22 22:53:29 +0600

    added user repository

---
+++ src/main/java/kg/edu/alatoo/game_store/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package kg.edu.alatoo.game_store.repository;
+
+import kg.edu.alatoo.game_store.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
