7db0350c61975985aa59a9bab59210da1ef3b61e
Давид Остапченко <dsxvqq@gmail.com>
2024-03-23 22:27:32 +0600

    deleted discount instance

--- src/main/java/kg/edu/alatoo/game_store/payload/game/GameRequest.java
+++ src/main/java/kg/edu/alatoo/game_store/payload/game/GameRequest.java
@@ -2,7 +2,6 @@ package kg.edu.alatoo.game_store.payload.game;
 
 public record GameRequest(
         String title,
-        Double price,
-        Integer discount
+        Double price
 ) {
 }
--- src/main/java/kg/edu/alatoo/game_store/payload/game/GameResponse.java
+++ src/main/java/kg/edu/alatoo/game_store/payload/game/GameResponse.java
@@ -3,7 +3,6 @@ package kg.edu.alatoo.game_store.payload.game;
 public record GameResponse(
         Long id,
         String title,
-        Double price,
-        Integer discount
+        Double price
 ) {
 }
