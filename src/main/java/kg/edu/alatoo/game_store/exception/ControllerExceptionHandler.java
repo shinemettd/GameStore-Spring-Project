b402bde8a52005d7720b0241f77137da94cb89cc
Давид Остапченко <dsxvqq@gmail.com>
2024-03-24 01:16:56 +0600

    added summarizing exception

--- src/main/java/kg/edu/alatoo/game_store/exception/ControllerExceptionHandler.java
+++ src/main/java/kg/edu/alatoo/game_store/exception/ControllerExceptionHandler.java
@@ -1,17 +1,36 @@
 package kg.edu.alatoo.game_store.exception;
 
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestControllerAdvice
 
 public class ControllerExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .toList();
+
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("time", LocalDateTime.now());
+        body.put("status", HttpStatus.BAD_REQUEST);
+        body.put("errors", errors);
+
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
+
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<Map> handleNotFound(NotFoundException ex) {
         Map<String, Object> body = new LinkedHashMap<>();
